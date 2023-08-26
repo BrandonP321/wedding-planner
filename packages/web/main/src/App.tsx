d7bfbbc52387c19f1819ad15f569c3e719ace596
Brandon Phillips <brandon.phillips@bphillips.dev>
2023-08-25 19:09:41 -0700

    Setup css vars

--- packages/web/main/src/App.tsx
+++ packages/web/main/src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Something</h1>
+      <h2>Something</h2>
+      <h3>Something</h3>
+      <h4>Something</h4>
+      <p>
+        Something <a href="asdf">Some link</a>
+      </p>
+      <small>Something</small>
     </div>
   );
 }
