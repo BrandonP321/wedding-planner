7da1f059cbd271fd1350e18e05ac5699b5d27aae
Brandon Phillips <brandon.phillips@bphillips.dev>
2023-09-11 13:04:22 -0700

    Fixed incorrect types for environmet variables

--- packages/cdk/lib/stacks/web/main/webMainEnv.ts
+++ packages/cdk/lib/stacks/web/main/webMainEnv.ts
@@ -1,10 +1,9 @@
 import { WebMainProcessEnv } from "@wedding-planner/shared/web/types/webMainEnv";
-import { DeploymentAccount } from "packages/cdk/lib/utils/accounts";
 import { Stage } from "../../../utils/types";
 import * as codebuild from "aws-cdk-lib/aws-codebuild";
 
 export type CDKEnvVars<T extends {}> = {
-  [name: string]: codebuild.BuildEnvironmentVariable;
+  [name in keyof T]: codebuild.BuildEnvironmentVariable;
 };
 
 export const getWebMainEnvVars = (
