e172eeb93f44ede3d7745527993ee0c6cc95a55f
Brandon Phillips <brandon.phillips@bphillips.dev>
2023-11-30 18:47:28 -0800

    Adapted filter query to account for multiple main choices

--- packages/api/src/controllers/vendor/searchVendorListings.controller.ts
+++ packages/api/src/controllers/vendor/searchVendorListings.controller.ts
@@ -12,21 +12,22 @@ const controller = new Controller<
 >(SearchVendorListingRequest.Errors);
 
 const minPriceSubQuery = `
-SELECT "mainChoiceId", SUM(min_price) AS total_min_price
-FROM (
+SELECT "mainChoice"."id" AS "mainChoiceId", SUM(min_price) AS "totalMinPricePerMainChoice"
+FROM "mainChoice"
+JOIN (
     SELECT "choiceGroup"."mainChoiceId", MIN("choice"."price") AS min_price
     FROM "choiceGroup"
     JOIN "choice" ON "choiceGroup"."id" = "choice"."choiceGroupId"
     GROUP BY "choiceGroup"."mainChoiceId", "choiceGroup"."id"
-) AS min_prices_per_group
-GROUP BY "mainChoiceId"
+) AS min_prices_per_group ON "mainChoice"."id" = min_prices_per_group."mainChoiceId"
+GROUP BY "mainChoice"."id"
 `;
 
-const requiredAttributes = [
+const requiredAttributes: string[] = [
   "traditional",
-  "photojournalistic",
-  "artistic",
-  "videography",
+  // "photojournalistic",
+  // "artistic",
+  // "videography",
 ];
 
 const attributeFilterSubQuery = `
@@ -59,7 +60,7 @@ export const SearchVendorListingsController = controller.handler(
 SELECT 
   "vendor"."id", 
   "vendor"."name", 
-  SUM("MinChoicePrices"."total_min_price") AS "TotalMinPrice"
+  MIN("MinChoicePrices"."totalMinPricePerMainChoice") AS "TotalMinPrice"
 FROM 
   "vendor"
 JOIN 
@@ -77,7 +78,7 @@ WHERE
 GROUP BY 
   "vendor"."id"
 HAVING 
-  SUM("MinChoicePrices"."total_min_price") <= 8000
+  MIN("MinChoicePrices"."totalMinPricePerMainChoice") <= 8000
 `;
 
     const vendorsWithMinPrice = await sequelize.query(rawQuery, {
