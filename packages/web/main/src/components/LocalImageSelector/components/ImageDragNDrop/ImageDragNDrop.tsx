2157acb394895df36a01a4f17378997f4642356f
Brandon Phillips <brandon.phillips@bphillips.dev>
2023-12-17 15:51:23 -0800

    Updated text & styling for image upload drag n drop

--- packages/web/main/src/components/LocalImageSelector/components/ImageDragNDrop/ImageDragNDrop.tsx
+++ packages/web/main/src/components/LocalImageSelector/components/ImageDragNDrop/ImageDragNDrop.tsx
@@ -1,7 +1,13 @@
 import styles from "../../LocalImageSelector.module.scss";
 import { useDropzone } from "react-dropzone";
-import { VendorMediaUtils } from "@wedding-planner/shared";
+import {
+  Button,
+  SpaceBetween,
+  VendorMediaUtils,
+} from "@wedding-planner/shared";
 import { useLocalImageSelector } from "components/LocalImageSelector/LocalImageSelectorProvider";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCloudArrowUp } from "@fortawesome/pro-solid-svg-icons";
 
 export type ImageDragNDropProps = {};
 
@@ -36,7 +42,19 @@ export const ImageDragNDrop = (props: ImageDragNDropProps) => {
   return (
     <div {...getRootProps({ className: styles.dropArea })}>
       <input {...getInputProps()} />
-      <p>Drag 'n' drop some files here, or click to select some files</p>
+      <SpaceBetween
+        classes={{ root: styles.uploadContent }}
+        size="m"
+        align="center"
+        vertical
+      >
+        <FontAwesomeIcon icon={faCloudArrowUp} className={styles.uploadIcon} />
+        <SpaceBetween align="center" stretch vertical>
+          <p>Drag & Drop to Upload Images</p>
+          <p>OR</p>
+          <Button variant="primary">Browse files</Button>
+        </SpaceBetween>
+      </SpaceBetween>
     </div>
   );
 };
